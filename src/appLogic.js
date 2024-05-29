16f7e7707404356ddf65807649da72cf3d246d1e
coed95 <codecoed95@gmail.com>
2024-05-29 17:39:04 +0200

    Add Projects to keep track of all projects: now Home, Today and Week are projects too

--- src/appLogic.js
+++ src/appLogic.js
@@ -1,3 +1,7 @@
+import Home from "../src/img/home.svg";
+import Today from "../src/img/today.svg";
+import Week from "../src/img/week.svg";
+
 export function Todo(title, description, dueDate, priority) {
     return {
         title,
@@ -8,9 +12,10 @@ export function Todo(title, description, dueDate, priority) {
     };
 }
 
-export function Project(name) {
+export function Project(name, defaultImage = "../src/img/list.svg") {
     return {
         name: name,
+        image: defaultImage,
         todos: [],
 
         addTodo: function(todo) {
@@ -43,3 +48,22 @@ export function Project(name) {
         },
     };
 }
+
+export const Projects = {
+    Home: Project("Home", Home),
+    Today: Project("Today", Today),
+    Week: Project("Week", Week),
+    Test: Project("Test"),
+    Test2: Project("Test 2"),
+    Test3: Project("Test 3"),
+
+    add: function(name) {
+        this[name] = Project(name);
+    },
+
+    delete: function(name) {
+        if (Object.keys(this).length > 3 && this.hasOwnProperty(name)) {
+            delete this[name];
+        }
+    },
+};
