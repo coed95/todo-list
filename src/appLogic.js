63bd84db30d1f6214f3ba87bae1e3d4efba67a95
coed95 <codecoed95@gmail.com>
2024-06-04 11:55:54 +0200

    Add a date check to ensure tasks are in the right project

--- src/appLogic.js
+++ src/appLogic.js
@@ -1,16 +1,30 @@
-import { isToday, isThisWeek, parseISO } from 'date-fns';
+import { parse, isToday, startOfWeek, endOfWeek, isWithinInterval } from 'date-fns';
 
 import Home from "../src/img/home.svg";
 import Today from "../src/img/today.svg";
 import Week from "../src/img/week.svg";
 
 export function Todo(title, description, dueDate, priority) {
+    const parsedDate = parse(dueDate, "dd-MM-yyyy", new Date());
+
     return {
         title,
         description,
         dueDate,
         priority,
         completed: false,
+
+        isDueToday: function() {
+            return isToday(this.dueDate);
+        },
+
+        isDueThisWeek: function() {
+            const today = new Date();
+            const start = startOfWeek(today);
+            const end = endOfWeek(today);
+
+            return isWithinInterval(this.dueDate, { start, end });
+        },
     };
 }
 
@@ -56,6 +70,18 @@ export const Projects = {
     deleteProject: function(name) {
         if (Object.keys(this).length > 3 && this.hasOwnProperty(name)) {
             delete this[name];
+        }
+    },
+
+    filterTask: function(project, todo) {
+        if (project === "home") {
+            if (todo.isDueThisWeek()) {
+                this["week"].addTodo(todo);
+
+                if (todo.isDueToday()) {
+                    this["today"].addTodo(todo);
+                }
+            }
         }
     },
 };
